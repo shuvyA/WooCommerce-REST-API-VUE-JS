b2574911c31d2b7c63310542c339b30a80932845
Shuvy <shuvyan@gmail.com>
2018-09-06 10:41:33 +0300

    Cleaner WooServise

--- src/services/WooService.js
+++ src/services/WooService.js
@@ -2,9 +2,7 @@ var WooCommerceAPI = require('woocommerce-api');
 
 import ConfigWoo from './ConfigWoo.js';
 
-
-
-var WooCommerce = new WooCommerceAPI({
+const WooCommerce = new WooCommerceAPI({
     url: 'https://shuvy.com/wooshop',
     consumerKey: ConfigWoo.consumerKey,
     consumerSecret: ConfigWoo.consumerSecret,
@@ -13,27 +11,11 @@ var WooCommerce = new WooCommerceAPI({
 });
 
 
-
-
-
-
-
 function products() {
-    // console.log(config.config);
     return WooCommerce.getAsync('products').then((result) => {
-        console.log(JSON.parse(result.toJSON().body));
-
         return JSON.parse(result.toJSON().body);
-
     });
-
-    // return resultProduct;
 }
-
-
-
-
-// console.log({ result });
 
 
 export default {
