f125e61c900832ce2a58cb1df37adf0964e440b4
Shuvy <shuvyan@gmail.com>
2018-09-06 10:22:20 +0300

    Add list and preview

---
+++ src/components/ProductList.vue
@@ -0,0 +1,26 @@
+<template>
+   <div class= "product-list" >
+<ul class="clean-list flex space-around align-center flex-wrap">
+    <li v-if="products" v-for="product in products" :key="product.id">
+    <product-preview :product="product"/>
+    </li>
+</ul>
+
+
+
+   </div>
+</template>
+
+<script>
+import ProductPreview from "@/components/ProductPreview.vue";
+export default {
+  name: "ProductList",
+  props: [ "products" ],
+  components: {
+    ProductPreview
+  }
+};
+</script>
+
+<style>
+</style>
