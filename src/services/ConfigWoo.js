bfe71d3c2f44f86b3f78499bc6231134ed2eff72
Shuvy Ankor <40235290+shuvyA@users.noreply.github.com>
2021-05-25 17:24:12 +0300

    remove

--- src/services/ConfigWoo.js
+++ src/services/ConfigWoo.js
@@ -1,12 +1,12 @@
 export default {
-  consumerKey: "ck_6bed38aa85b438ab9dcb3e18dd150011b407384c",
-  consumerSecret: "cs_3731e657dcdbd382bb89a88d1e0b3954df8ef0ed"
+  consumerKey: "exmple",
+  consumerSecret: "example"
 };
 
 // var WooCommerce = new WooCommerceAPI({
-//     url: 'https://shuvy.com/wooshop',
-//     consumerKey: 'ck_6bed38aa85b438ab9dcb3e18dd150011b407384c',
-//     consumerSecret: 'cs_3731e657dcdbd382bb89a88d1e0b3954df8ef0ed',
+//     url: 'example',
+//     consumerKey: 'example',
+//     consumerSecret: 'example',
 //     wpAPI: true,
 //     version: 'wc/v1'
 // });
